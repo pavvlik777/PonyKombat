326307ae624a1268371717a436eedaeac2490fb4
pavvlik777 <41309600+pavvlik777@users.noreply.github.com>
2019-03-21 21:32:50 +0300

    added easter egg and files for heroes data

---
+++ PonyKombat/Assets/Scripts/Game/Heroes/HeroController/ControlFSM/HitState.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace n_Game.Combat.Control
+{
+	public class HitState : State
+	{
+		public override Vector3 LeaveState(StatesNames newState)
+		{
+			return Vector3.zero;
+		}
+		public override void EnterState (Vector3 oldMoveDirection)
+		{
+			m_MoveDirection.y = oldMoveDirection.y;
+		}
+		public override void FixedUpdateState(out Vector3 moveDirection)
+		{
+			moveDirection = Vector3.zero;
+		}
+
+		public void HitFinished()
+		{
+			m_ControlFSM.ChangeState(StatesNames.Walk);
+		}
+	}
+}
---
+++ PonyKombat/Assets/Scripts/MainClasses/GameVideo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GameVideo
+{
+	public static Resolution ScreenResolution;
+
+	public static void SetDefaultSettings() //TBD
+	{
+		Screen.SetResolution(1600, 900, true);
+		GameVideo.ScreenResolution = Screen.currentResolution;
+	}
+}
